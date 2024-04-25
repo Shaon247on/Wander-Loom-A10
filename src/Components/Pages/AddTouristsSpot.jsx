ec1def09ebd081d85ce8fccb9db3d07007143f51
Md. Aminul Islam Shaon <aminul007.ais@gmail.com>
2024-04-26 05:17:51 +0600

    taking break for lunch

---
+++ src/Components/Pages/AddTouristsSpot.jsx
@@ -0,0 +1,9 @@
+const AddTouristsSpot = () => {
+    return (
+        <div>
+            <h2 className="text-2xl text-center">Update a touring place</h2>
+        </div>
+    );
+};
+
+export default AddTouristsSpot;
