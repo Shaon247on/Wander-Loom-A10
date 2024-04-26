354d9e2ef7cf1aa1469d9ec27f34c1908a46d34a
shaon <aminul007.ais@gmail.com>
2024-04-26 08:57:03 +0600

    fixed all issue and added home page

---
+++ src/Components/Pages/Home.jsx
@@ -0,0 +1,10 @@
+const Home = () => {
+    return (
+        <div>
+            <h1 className="text-4xl font-bold text-center">This is home</h1>
+            <p>hello</p>
+        </div>
+    );
+};
+
+export default Home;
--- src/Routes/MainRoute.jsx
+++ src/Routes/MainRoute.jsx
@@ -1,10 +1,18 @@
 import { createBrowserRouter } from "react-router-dom";
+import Home from "../Components/Pages/Home";
+import Root from "../Components/Pages/Root";
 
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <div>Hello world!</div>,
+        element: <Root></Root>,
+        children:[
+            {
+                path: '/',
+                element: <Home></Home>
+            }
+        ]
     },
 ]);
 export default router;
